e77e7d89d488f206e7c2bdd80b93bb82d5bde669
gallaugher <john.gallaugher@bc.edu>
2025-01-12 19:27:12 -0500

    1.8 After adding image to app

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I Am A Programmer!"
+    @State private var message = ""
+    @State private var imageString = ""
     var body: some View {
         
         VStack {
             Spacer()
             
-            Image(systemName: "swift")
+            Image(systemName: imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-                .frame(width: 200, height: 200)
             
             Text(message)
                 .font(.largeTitle)
@@ -26,14 +26,23 @@ struct ContentView: View {
             
             Spacer()
             
-            HStack {
-                Button("Awesome") {
-                    message = "Awesome!"
-                }
+            
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
                 
-                Button("Great") {
-                    message = "Great!"
-                }
+//                if message == message1 {
+//                    message = message2
+//                    imageString = imageString2
+//                } else {
+//                    message = message1
+//                    imageString = imageString1
+//                }
+                
+                message = ( message == message1 ? message2 : message1 )
+                imageString = ( imageString == imageString1 ? imageString2 : imageString1 )
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
